8c0a7921df720446bb1590483532853ee26c1a0e
derekn4 <derekn4@uci.edu>
2021-01-27 21:20:29 -0800

    Initial Commit

---
+++ UsHealth/UsHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsHealthApp.swift
+//  UsHealth
+//
+//  Created by Derek Nguyen on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct UsHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
