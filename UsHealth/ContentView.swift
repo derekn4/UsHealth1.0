18fed1293cc303de8a37b77c6cb605736f445fa4
derekn4 <derekn4@uci.edu>
2021-02-09 14:15:36 -0800

    Google Sign in Completed

--- UsHealth/ContentView.swift
+++ UsHealth/ContentView.swift
@@ -6,16 +6,97 @@
 //
 
 import SwiftUI
+import GoogleSignIn
+import Firebase
 
 struct ContentView: View {
+    
+    @ObservedObject var info : AppDelegate
+    @State var user = Auth.auth().currentUser
+    
     var body: some View {
-        Text("Hello, world! Testing")
-            .padding()
+        VStack{
+            if user != nil {
+                Home()
+            }
+            else {
+                OnBoard()
+            }
+        }
+        .onAppear {
+            NotificationCenter.default.addObserver(forName: NSNotification.Name("SIGNIN"), object: nil, queue: .main) { (_) in
+                self.user = Auth.auth().currentUser
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct OnBoard : View {
+    
+    @State var onBoard = [
+      
+              Board(title: "Data Analysis", detail: "Data analysis is a process of inspecting, cleansing, transforming and modeling data with the goal of discovering useful information, informing conclusions and supporting decision-making.", pic: "b1"),
+              
+              Board(title: "Social Media", detail: "Social media are interactive computer-mediated technologies that facilitate the creation or sharing of information, ideas, career interests and other forms of expression via virtual communities and networks.", pic: "b2"),
+              
+              Board(title: "Software Development", detail: "Software development is the process of conceiving, specifying, designing, programming, documenting, testing, and bug fixing involved in creating and maintaining applications, frameworks, or other software components.", pic: "b3"),
+      
+          ]
+
+    @State var  index = 0
+    var body: some View {
+        VStack{
+            Image(self.onBoard[self.index].pic).resizable()
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2)
+            HStack(spacing: 12){
+                ForEach(0..<self.onBoard.count, id: \.self){ i in
+                    Circle().fill(self.index == i ? Color.gray : Color.black.opacity(0.07))
+                        .frame(width: 10, height: 10)
+                }
+            }.padding(.top, 30)
+            Text(self.onBoard[self.index].title).font(.title).fontWeight(.bold).foregroundColor(.black).padding(.top, 35)
+            Text(self.onBoard[self.index].detail).foregroundColor(.black).padding(.top).padding(.horizontal, 20)
+            
+            Spacer(minLength: 0)
+            
+            Button(action: {
+                if self.index < self.onBoard.count - 1{
+                    self.index+=1
+                }
+                else{
+                    GIDSignIn.sharedInstance()?.presentingViewController = UIApplication.shared.windows.first?.rootViewController
+                    
+                    GIDSignIn.sharedInstance()?.signIn()
+                }
+            }) {
+                HStack(spacing: 30){
+                    if self.index == self.onBoard.count - 1{
+                        Image("google").resizable().renderingMode(.template)
+                            .foregroundColor(.white)
+                            .frame(width: 25, height: 25)
+                    }
+                    
+                    Text(self.index == self.onBoard.count - 1 ? "Login" : "Next")
+                        .foregroundColor(.white).fontWeight(.bold)
+                }.padding(.vertical)
+                .frame(width: UIScreen.main.bounds.width - 200).background(Color.red).clipShape(Capsule())
+            }
+            
+        }.edgesIgnoringSafeArea(.top)
     }
+}
+
+struct Home : View {
+    var body: some View {
+        VStack{
+            Text("Logged in As  \n\(Auth.auth().currentUser!.email ?? "No email")").multilineTextAlignment(.center)
+            Text("Start Dashboard View Setup")
+        }
+    }
+}
+
+struct Board {
+    var title : String
+    var detail : String
+    var pic : String
 }
--- UsHealth/UsHealthApp.swift
+++ UsHealth/UsHealthApp.swift
@@ -6,12 +6,69 @@
 //
 
 import SwiftUI
+import Firebase
+import GoogleSignIn
 
 @main
 struct UsHealthApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(info: self.delegate)
         }
     }
 }
+
+//To Observer or read data from app delegate
+
+class AppDelegate: NSObject, UIApplicationDelegate, GIDSignInDelegate, ObservableObject {
+    @Published var email = ""
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+            [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        //Initialize Firebase
+        FirebaseApp.configure()
+        
+        //Initialize Google
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
+        
+        return true
+      }
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        if error != nil {
+            print(error.localizedDescription)
+            return
+        }else {
+            print("Log Out for \(self.email) success!")
+        }
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        guard let user = user else {return}
+        
+        let credential = GoogleAuthProvider.credential(withIDToken: user.authentication.idToken,
+                                                       accessToken: user.authentication.accessToken)
+        //Signin to Firebase
+        Auth.auth().signIn(with: credential) { (result, error) in
+            if error != nil {
+                print((error?.localizedDescription)!)
+                return
+            }
+
+            
+            NotificationCenter.default.post(name: NSNotification.Name("SIGNIN"),  object: nil)
+            
+            self.email = (result?.user.email)!
+            print(result?.user.email)
+            
+        }
+    
+    }
+
+}
+
